c740bc35b905b1ad9c83185b4db415c2f3a99ada
Brian Lauber <constructible.truth@gmail.com>
2011-07-19 20:55:13 -0400

    First commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name='pytee',
+    version='0.1',
+    description="It's a pytee that Python does not natively support stream multiplexing.",
+    author="Brian Lauber",
+    author_email="constructible.truth@gmail.com",
+    url='https://github.com/briandamaged/pytee',
+    packages=['pytee'],
+)
